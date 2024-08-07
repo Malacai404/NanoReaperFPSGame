3e84b8dd5d1123c960fc33486af9ee4c11577c2b
Malacai404 <malacai404@gmail.com>
2024-08-07 13:14:53 +1200

    hampter

---
+++ Scripts/BloodOrb.gd
@@ -0,0 +1,22 @@
+extends RigidBody3D
+
+var blood_splat = preload("res://Scenes/blood.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	linear_velocity.x = randf_range(-7, 7)
+	linear_velocity.z = randf_range(-7, 7)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_body_entered(body):
+	if(body.name == "floor"):
+		var blood_object = blood_splat.instantiate()
+		blood_object.position= Vector3(position.x, position.y -0.15, position.z)
+		blood_object.rotation_degrees.y = randf_range(-90,90)
+		get_parent().add_child(blood_object)
+		queue_free()
---
+++ Scripts/hamster.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+var blood_ball = preload("res://blood_orb.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _spurt_blood():
+	var blood_ball_object = blood_ball.instantiate()
+	blood_ball_object.position = position
+	get_parent().add_child(blood_ball_object)
+func _on_hamster_area_body_entered(body):
+	if(body.name == "Player"):
+		if(body.is_dashing == true):
+			for i in range(10):
+				_spurt_blood()
+			body.dark_energy_value += 10
+			queue_free()
